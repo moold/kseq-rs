50c182a5c88d954608fe229ac907b223c5d72485
Moold <mooldhu@gmail.com>
2022-06-20 18:03:56 +0800

    fixed lints reported by cargo clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,19 @@
 #![doc = include_str!("../README.md")]
 // Note: kseq is inspired by fastq-rs and kseq in C
 
-use std::io::{Result, Read, BufRead, BufReader, Error, ErrorKind, Cursor};
 use flate2::read::MultiGzDecoder;
+use std::io::{BufRead, BufReader, Cursor, Error, ErrorKind, Read, Result};
 use std::path::Path;
-use atty;
 
 pub mod record;
-use record::{Reader, Readers, Fastx, Result as ParseResult};
+use record::{Fastx, Reader, Readers, Result as ParseResult};
 
 // A wrapper that allows parse_path to accept Option<String>, String and &str as parameter
 pub struct Wrapper(String);
 
 impl From<Option<String>> for Wrapper {
     fn from(path: Option<String>) -> Self {
-        Wrapper(path.unwrap_or("-".into()))
+        Wrapper(path.unwrap_or_else(|| "-".into()))
     }
 }
 
@@ -33,7 +32,7 @@ impl From<&str> for Wrapper {
 /// Reader for a single path or Readers for multiple paths
 pub enum Paths {
     Reader(Reader),
-    Readers(Readers)
+    Readers(Readers),
 }
 
 impl Paths {
@@ -48,7 +47,8 @@ impl Paths {
 
 /// parse path to a Reader or Readers
 pub fn parse_path<T>(path: T) -> Result<Paths>
-    where T: Into<Wrapper>
+where
+    T: Into<Wrapper>,
 {
     let path = path.into().0;
     let mut reader: Box<dyn BufRead> = if path == "-" {
@@ -56,14 +56,15 @@ pub fn parse_path<T>(path: T) -> Result<Paths>
             return Err(Error::new(ErrorKind::InvalidInput, "Missing input"));
         }
         Box::new(BufReader::with_capacity(65536, std::io::stdin()))
-    }else {
+    } else {
         Box::new(BufReader::with_capacity(65536, std::fs::File::open(&path)?))
     };
 
     let mut format_bytes = [0u8; 4];
     reader.read_exact(&mut format_bytes)?;
     reader = Box::new(Cursor::new(format_bytes.to_vec()).chain(reader));
-    if &format_bytes[..2] == b"\x1f\x8b" {// for gz foramt
+    if &format_bytes[..2] == b"\x1f\x8b" {
+        // for gz foramt
         reader = Box::new(BufReader::with_capacity(65536, MultiGzDecoder::new(reader)));
         format_bytes.iter_mut().for_each(|m| *m = 0);
         reader.read_exact(&mut format_bytes)?;
@@ -71,31 +72,29 @@ pub fn parse_path<T>(path: T) -> Result<Paths>
     }
 
     match format_bytes[0] {
-        b'@' | b'>' => {
-            Ok(Paths::Reader(Reader::new(reader)))
-        }
-        _ => {// for a fofn file
+        b'@' | b'>' => Ok(Paths::Reader(Reader::new(reader))),
+        _ => {
+            // for a fofn file
             let mut paths = Readers::new();
-            let _path = if path == "-" {
-                ""
-            }else{
-                &path
-            };
+            let _path = if path == "-" { "" } else { &path };
             let parent = Path::new(&_path).parent().unwrap_or_else(|| Path::new(""));
 
-            for _line in reader.lines().map(|l| l.unwrap()){
+            for _line in reader.lines().map(|l| l.unwrap()) {
                 let line = _line.trim();
-                if line.starts_with('#') || line.is_empty(){
+                if line.starts_with('#') || line.is_empty() {
                     continue;
                 }
-                let _path = parent.join(line);// convert to a absolute path
-                if _path.exists(){
+                let _path = parent.join(line); // convert to a absolute path
+                if _path.exists() {
                     match parse_path(Some(_path.to_str().unwrap().to_string()))? {
                         Paths::Reader(reader) => paths.readers.push(reader),
-                        _ => unreachable!()
+                        _ => unreachable!(),
                     }
-                }else{
-                    return Err(Error::new(ErrorKind::InvalidData, format!("{:?} is not a valid fastq/fasta/fofn file", _path)))
+                } else {
+                    return Err(Error::new(
+                        ErrorKind::InvalidData,
+                        format!("{:?} is not a valid fastq/fasta/fofn file", _path),
+                    ));
                 }
             }
             Ok(Paths::Readers(paths))
